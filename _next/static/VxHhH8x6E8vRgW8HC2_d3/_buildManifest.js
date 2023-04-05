c9a3a6316932debd8d15eeb649fb18ac765b8e2d
Hrushikesh Patel <hrushikesh592002@gmail.com>
2023-04-06 00:58:17 +0530

    Deploy gh-pages

--- _next/static/VxHhH8x6E8vRgW8HC2_d3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/315-51a2837868a00b93.js","static/chunks/pages/index-17366bcf5e4911e2.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
