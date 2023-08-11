d34e14d5ccd4b6f7f6a312c57874440593208a71
Hrushikesh Patel <hrushikesh592002@gmail.com>
2023-08-11 18:32:38 +0530

    Deploy gh-pages

---
+++ _next/static/7-upOhgA5mXmNrSKqezPb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0a4ec652d6028205.css","static/chunks/337-99192bb1eb06fbdc.js","static/chunks/pages/index-42779c8b81f63aa8.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/GDhFE8bfQim0D_0Xwgu7B/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0a4ec652d6028205.css","static/chunks/337-7331acc1969ebb25.js","static/chunks/pages/index-af12550c25328fe4.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
